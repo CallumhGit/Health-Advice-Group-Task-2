1d295ea37feaffc1067a1677f50f9cb74e2c1a4d
CallumhGit <callumh047@gmail.com>
2024-02-12 14:34:02 +0000

    seperated scripts into own files

---
+++ static/js/navbarScript.js
@@ -0,0 +1,11 @@
+  // Get the navigation links element
+  var navLinks = document.getElementById('navLinks');
+// Function to show the navigation links
+  function showMenu() {
+    navLinks.style.right = "0";
+  }
+
+  // Function to hide the navigation links
+  function hideMenu() {
+    navLinks.style.right = '-200px';
+  }
